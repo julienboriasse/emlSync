1e7224446a5c82d52154949fef1cb98db1d9e156
Julien Boriasse <julien@boriasse.com>
2022-06-01 11:18:09 -0400

    Format

--- main.py
+++ main.py
@@ -1,33 +1,48 @@
 import datetime
 import json
 import eml_parser
-
+import os
 import sqlite3 as sl
 
+# EML Parser object
+ep = eml_parser.EmlParser()
+
+# Connect to the local database
 con = sl.connect('emlSync.db')
 
+# Create database transfer table if not existing
+# #TODO Check if database structure is up to date
 with con:
     con.execute("""
         CREATE TABLE IF NOT EXISTS emlTransfers (
             id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
             datetime TIMESTAMP NOT NULL ,
-            eml TEXT NOT NULL,
+            full_path TEXT NOT NULL,
+            subject TEXT NOT NULL,
+            email_datetime TEXT NOT NULL,
             status INTEGER
         );
     """)
 
+# Get all the eml files from the directory
+emlFiles = []
+for root, dirs, files in os.walk(r"/Users/julien/pythonProject/emlSync"):
+    for file in files:
+        if file.endswith(".eml"):
+            emlFiles.append(os.path.join(root, file))
+
+print(str(len(emlFiles)) + " emails found in the directory")
 
 
-with open('test/0_HJqO8GiDxgfkCfd2EWk-PV5RauFjgRHWKp5S6Qo6AjscIKsEzQXjaxdlq07v4KqC-Jij0jAWXTnivO6SiRcwDw==.eml', 'rb') as fhdl:
-    raw_email = fhdl.read()
 
 
-ep = eml_parser.EmlParser()
-parsed_eml = ep.decode_email_bytes(raw_email)
-
-
-print(parsed_eml['header']['subject'])
-print(parsed_eml['header']['date'])
+for emlFile in emlFiles:
+    with open(emlFile, 'rb') as fhdl:
+        raw_email = fhdl.read()
+    parsed_eml = ep.decode_email_bytes(raw_email)
+    print(parsed_eml['header']['subject'])
+    print(parsed_eml['header']['date'])
+    print(" ")
 
 
 
