d9ca47f59cf1e7c5842cb0ce2a943e3131184de4
Julien Boriasse <julien@boriasse.com>
2022-06-01 10:46:19 -0400

    Create SQLite DB and table

--- main.py
+++ main.py
@@ -2,11 +2,20 @@ import datetime
 import json
 import eml_parser
 
+import sqlite3 as sl
 
-def json_serial(obj):
-    if isinstance(obj, datetime.datetime):
-        serial = obj.isoformat()
-        return serial
+con = sl.connect('emlSync.db')
+
+with con:
+    con.execute("""
+        CREATE TABLE IF NOT EXISTS emlTransfers (
+            id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+            datetime TIMESTAMP NOT NULL ,
+            eml TEXT NOT NULL,
+            status INTEGER
+        );
+    """)
+
 
 
 with open('test/0_HJqO8GiDxgfkCfd2EWk-PV5RauFjgRHWKp5S6Qo6AjscIKsEzQXjaxdlq07v4KqC-Jij0jAWXTnivO6SiRcwDw==.eml', 'rb') as fhdl:
